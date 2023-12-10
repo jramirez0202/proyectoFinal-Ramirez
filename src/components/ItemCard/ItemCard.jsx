5163badd12f1a42536bed506af6e1ccfd43ee5a6
jramirez0202 <johkcolom@gmail.com>
2023-12-10 19:14:56 -0300

    Lista, filtro y detalle de productos de la segunda pre entrega

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,93 +2,106 @@ import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-
-const ItemCard = ({name, price, category, img, id }) => {
+const ItemCard = ({ name, price, category, img, id }) => {
   const { t } = useTranslation();
   return (
-      <div className="w-full mx-auto max-w-sm  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <div className="flex justify-center text-center">
-					<h5 className="text-2xl font-semibold  mt-2 tracking-tight text-gray-900 dark:text-white">
-            {t(`itemCard.card.productName.p${id}`) || name}
-					</h5>
-				</div>
-        <figure>
-          <img
-            className="p-8 rounded-t-lg"
-            src={img}
-            alt={name}
-          />
-        </figure>
+    <div className="w-full mx-auto max-w-sm  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex justify-center text-center">
+        <h5 className="text-2xl font-semibold  mt-2 tracking-tight text-gray-900 dark:text-white">
+          {t(`itemCard.card.productName.p${id}`) || name}
+        </h5>
+      </div>
+      <figure>
+        <img className="p-8 rounded-t-lg" src={img} alt={name} />
+      </figure>
 
-				<div className="mt-2.5 mb-5 ml-5">
-					<span className="text-xl font-bold text-gray-900 dark:text-white"> {t('itemCard.card.price')} ${price}</span>
-          <br />
-          <span className="text-xl font-bold text-gray-900 dark:text-white"> {t('itemCard.card.category')} {t(`itemCard.card.categoryName.c${id}`) || category}</span>
-          <div className="flex items-center mt-2.5 mb-5">
-            <div className="flex items-center space-x-1 rtl:space-x-reverse">
+      <div className="mt-2.5 mb-5 ml-5">
+        <span className="text-xl font-bold text-gray-900 dark:text-white">
+          {" "}
+          {t("itemCard.card.price")} ${price}
+        </span>
+        <br />
+        <span className="text-xl font-bold text-gray-900 dark:text-white">
+          {" "}
+          {t("itemCard.card.category")}{" "}
+          {t(`itemCard.card.categoryName.c${id}`) || category}
+        </span>
+        <div className="flex items-center mt-2.5 mb-5">
+          <div className="flex items-center space-x-1 rtl:space-x-reverse">
+            <svg
+              className="w-4 h-4 text-yellow-300"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-4 h-4 text-yellow-300"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-4 h-4 text-yellow-300"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-4 h-4 text-yellow-300"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+            <svg
+              className="w-4 h-4 text-gray-200 dark:text-gray-600"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 22 20"
+            >
+              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+            </svg>
+          </div>
+          <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+            5.0
+          </span>
+        </div>
+        <div className="flex justify-end mr-6">
+          <Link to={`/item/${id}`}>
+            <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              {t("itemCard.card.detailsButton")}
               <svg
-                className="w-4 h-4 text-yellow-300"
+                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
+                fill="none"
+                viewBox="0 0 14 10"
               >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-yellow-300"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg
-                className="w-4 h-4 text-gray-200 dark:text-gray-600"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 22 20"
-              >
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
               </svg>
             </div>
-            <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-              5.0
-            </span>
-          </div>
-          <div className="flex justify-end mr-6">
-            <Link to={`/item/${id}`}>
-              <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              {t('itemCard.card.detailsButton')}
-                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-              </div>
-            </Link>
-          </div>
-          
-				</div>
+          </Link>
+        </div>
       </div>
+    </div>
   );
 };
 
@@ -98,7 +111,7 @@ ItemCard.propTypes = {
   name: PropTypes.string,
   price: PropTypes.number,
   stock: PropTypes.number,
-  category: PropTypes.string
+  category: PropTypes.string,
 };
 
 export default ItemCard;
--- src/components/ItemCardDetail/ItemCardDetail.jsx
+++ src/components/ItemCardDetail/ItemCardDetail.jsx
@@ -1,38 +1,53 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import ItemCount from "../ItemCount/ItemCount";
 
-
-const ItemCardDetail = ({name, price, stock, description, category, img, id }) => {
+const ItemCardDetail = ({
+  name,
+  price,
+  stock,
+  description,
+  category,
+  img,
+  id,
+}) => {
   const { t } = useTranslation();
   return (
-    
-<div className=" mx-auto my-auto mt-10 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img className="rounded-t-lg" src={img} alt={name}/>
-    </a>
-    <div className="p-5">
-  <div className="flex flex-col items-start">
-    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{t('itemCard.card.name')} {t(`itemCard.card.productName.p${id}`) || name}</h5>
-    <h5 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">{t('itemCard.card.price')} {price}</h5>
-    <h5 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">{t('itemCard.card.category')} {t(`itemCard.card.categoryName.c${id}`) || category}</h5>
-  </div>
+    <div className=" mx-auto my-auto mt-10 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img className="rounded-t-lg" src={img} alt={name} />
+      </a>
+      <div className="p-5">
+        <div className="flex flex-col items-start">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {t("itemCard.card.name")}{" "}
+            {t(`itemCard.card.productName.p${id}`) || name}
+          </h5>
+          <h5 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">
+            {t("itemCard.card.price")} {price}
+          </h5>
+          <h5 className="mb-2 text-xl font-bold text-gray-900 dark:text-white">
+            {t("itemCard.card.category")}{" "}
+            {t(`itemCard.card.categoryName.c${id}`) || category}
+          </h5>
+        </div>
 
-    <p className="flex justify-center mb-3 font-normal text-gray-700 dark:text-gray-400">{t(`itemCard.card.productDescription.product_${id}`) || description}</p>
+        <p className="flex justify-center mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {t(`itemCard.card.productDescription.product_${id}`) || description}
+        </p>
         <div className="flex justify-center items-center">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">
-              <ItemCount
-                initial={1}
-                stock={stock}
-                onAdd={(quantity) => console.log(quantity)}
-              />
-            </span>
-          </div>
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+            <ItemCount
+              initial={1}
+              stock={stock}
+              onAdd={(quantity) => console.log(quantity)}
+            />
+          </span>
+        </div>
+      </div>
     </div>
-</div>
-
-  )
-}
+  );
+};
 
 ItemCardDetail.propTypes = {
   onAdd: PropTypes.func,
@@ -42,6 +57,6 @@ ItemCardDetail.propTypes = {
   stock: PropTypes.number,
   category: PropTypes.string,
   description: PropTypes.string,
-}
+};
 
-export default ItemCardDetail
+export default ItemCardDetail;
